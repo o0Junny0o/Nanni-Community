9bde92452fd8d43ec69028c90198cecc37756fe6
CoracaoDeLeao <isaacdecarvalho2005@gmail.com>
2025-03-19 10:46:30 -0300

    :tada: Commit inicial

---
+++ Nanni/eslint.config.mjs
@@ -0,0 +1,58 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactNative from 'eslint-plugin-react-native';
+import prettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'], // Applies to JS and JSX files
+    languageOptions: {
+      globals: {
+        ...globals.node, // Adds Node.js globals
+        ...globals.browser, // Adds browser globals (useful for React Native)
+        __DEV__: 'readonly',
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // Enable JSX parsing
+        },
+        ecmaVersion: 'latest', // Use the latest ECMAScript version
+        sourceType: 'module', // Use ES modules
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+  },
+  js.configs.recommended, // Recommended config for JavaScript
+  {
+    plugins: {
+      react: react, // Plugin object for React
+    },
+    rules: {
+      ...react.configs.recommended.rules, // Include recommended React rules
+    },
+  },
+  {
+    plugins: {
+      'react-native': reactNative, // Plugin object for React Native
+    },
+    rules: {
+      'react-native/no-unused-styles': 'error', // Prevents unused styles
+      'react-native/split-platform-components': 'error', // Splits components by platform
+      'react-native/no-inline-styles': 'error', // Prevents inline styles
+      'react-native/no-color-literals': 'error', // Prevents color literals
+      'react/react-in-jsx-scope': 'off', // No need to import React with JSX
+      'no-console': 'warn', // Warns for console.log()
+    },
+  },
+  {
+    rules: {
+      ...prettier.rules, // Disable conflicting rules for Prettier
+    },
+  },
+];
