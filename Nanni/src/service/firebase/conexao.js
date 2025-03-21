1b3a05ca47309ddba88accaefc5296655bd7c1c0
CoracaoDeLeao <isaacdecarvalho2005@gmail.com>
2025-03-21 11:10:48 -0300

    feat(auth): Adicionar lógica para autenticação de usuários
    Implementa login e cadastro utilizando Firebase Authentication, incluindo validação de entrada e tratamento de erros.

---
+++ Nanni/src/service/firebase/conexao.js
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig.extra.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.FIREBASE_APP_ID,
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa o Firebase Auth com persistência
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export { app, auth };
