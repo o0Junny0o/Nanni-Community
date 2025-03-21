535a7d1dd938ebbf0e974a5870fc3f6ca7df4442
CoracaoDeLeao <isaacdecarvalho2005@gmail.com>
2025-03-21 10:34:39 -0300

    Revert "feat(auth): adicionar lógica para autenticação de usuários"
    This reverts commit b2642a8bd5e4520885a6a930559401f472bed923.
    
    :wastebasket: remove: Removendo arquivos sensíveis

--- Nanni/App.js
+++ Nanni/App.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-// Importa as telas
-import Login from './src/app/screen/LOGIN_SCREEN/index';
-import Cadastro from './src/app/screen/CADASTRO_SCREEN/index';
-
-// Criação do Stack Navigator
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Cadastro"
-          component={Cadastro}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- Nanni/app.config.js
+++ Nanni/app.config.js
@@ -2,37 +2,30 @@ require('dotenv').config();
 
 export default {
   expo: {
-    name: 'Nanni',
-    slug: 'Nanni',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './src/assets/icon.png',
-    userInterfaceStyle: 'light',
+    name: "Nanni",
+    slug: "Nanni",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
     newArchEnabled: true,
     splash: {
-      image: './src/assets/splash-icon.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
     },
     ios: {
-      supportsTablet: true,
+      supportsTablet: true
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: './src/assets/adaptive-icon.png',
-        backgroundColor: '#ffffff',
-      },
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
     },
     web: {
-      favicon: './src/assets/favicon.png',
+      favicon: "./assets/favicon.png"
     },
-    extra: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    },
+    extra: {},
   },
 };
